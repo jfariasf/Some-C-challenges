9357ce920d5009115ae5dcb669a9e8d2043c5340
Javier F. Arias <752@holbertonschool.com>
2019-02-06 18:15:50 +0000

    Adding 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Prints "Programming is like building a multilingual puzzle
+ * main - Prints "with proper grammar, but the outcome is a piece of art,"
  *
  * Return: Always 0 (Success)
  */
