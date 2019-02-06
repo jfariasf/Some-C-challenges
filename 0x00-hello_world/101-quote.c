23fc836ffd352990c0cf31b856f4c1bad30f7c0d
Javier F. Arias <752@holbertonschool.com>
2019-02-06 21:46:10 +0000

    Fixing 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,7 @@
 
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, msg, sizeof(msg)-1);
 	return (1);
 }
