44e84c4e5cd4aae98fafbc2f33f7aa7a57483126
Javier F. Arias <752@holbertonschool.com>
2019-02-06 19:57:52 +0000

    Fixing 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 /**
- * main - Prints a message to the standar error
+ * main - Prints a message to the standard error
  *
  * Return: Always 1 (Success)
  */
 
 int main(void)
 {
-	char message[] = "and that piece of art is useful"
-	       " - Dora Korpar, 2015-10-19\n";
+	char message_stderr[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	fwrite(message, 1, sizeof(message), stderr);
+	fwrite(&message_stderr, 1, sizeof(message_stderr), stderr);
 	return (1);
 }
