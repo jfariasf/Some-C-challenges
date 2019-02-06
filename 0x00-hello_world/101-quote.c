ae3e1a55b0bb3c8032933334724c34bfb54ff1af
Javier F. Arias <752@holbertonschool.com>
2019-02-06 19:36:06 +0000

    Adding 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Prints a message to the standar error
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	fwrite(message, 1, sizeof(message), stderr);
+	return (1);
+}
