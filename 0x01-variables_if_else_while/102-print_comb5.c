e6316c2c9fc02fd8ffcbbfd79c1506e165c17986
Javier F. Arias <752@holbertonschool.com>
2019-02-07 19:52:47 +0000

    Trying 102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+/**
+* main - Prints the single digit integers
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+	int l;
+
+	for (i = 0x30; i <= 0x39; i++)
+	{
+		for (j = 0x30; j <= 0x39; j++)
+		{
+			for (k = i; k <= 0x39; k++)
+			{
+				for (l = j; l <= 0x39; l++)
+				{
+					if ((i + j != k + l))
+					{
+						if (i > 0x30 || j > 0x30 ||
+							k > 0x30 || l > 0x31)
+						{
+							putchar(',');
+							putchar(' ');
+						}
+						putchar(i);
+						putchar(j);
+						putchar(' ');
+						putchar(k);
+						putchar(l);
+					}
+				}
+			}
+		}
+	}
+	putchar(0x0A);
+	return (0);
+}
