4122cf05f4bbd8384f1bfe174c88e84167d80cd9
Javier F. Arias <752@holbertonschool.com>
2019-02-07 19:59:31 +0000

    Uploading 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+* main - Prints the single digit integers
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0x30; i <= 0x39; i++)
+	{
+		for (j = i; j <= 0x39; j++)
+		{
+			if (i != j)
+			{
+				if ((i > 0x30) || (j > 0x31))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+					putchar(i);
+					putchar(j);
+			}
+		}
+	}
+	putchar(0x0A);
+	return (0);
+}
