302e0218e0feadc57829019ede3691572daccb4e
Javier F. Arias <752@holbertonschool.com>
2019-02-12 07:29:09 -0500

    Uploading header file

---
+++ 0x02-functions_nested_loops/holberton.h
@@ -0,0 +1,19 @@
+//insertion_sort.h file
+#ifndef _holberton_h
+#define _holberton_h
+#endif 
+/*Sorts an integer array. Takes a pointer to the first element and the length of the array as input. 
+Returns 0 on successful sort.*/
+int _putchar(int); 
+int _islower(int);
+void print_alphabet_x10(void);
+void print_alphabet(void);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
