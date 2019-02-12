744c3efdc763438cc50d6b08b8f41dc6583c6e3c
Javier F. Arias <752@holbertonschool.com>
2019-02-12 22:23:47 +0000

    Uploading 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <limits.h>
 /**
 * main - Prints the first 98 fibonacci numbers separated by a comma and a space
 *
@@ -6,21 +7,44 @@
 */
 int main(void)
 {
-	double j = 1;
-	double k = 1;
-	double tmp;
-	int start = 0;
 	int i;
+	unsigned long k=1;
+	unsigned long j=1;
+	unsigned long tmp;
+	unsigned long partial1;
+	unsigned long partial2;
+	int partialresult;
+	int digittmp;
+	int start = 0;
 
 	for (i = 0; i < 98; i++)
 	{
 		if (start)
 			printf(", ");
-		printf("%.0f", k);
+		if (k > 100000000000000000){
+			partial1 = k % 10;
+			partial2 = j % 10;			
+			k /= 10;
+			j /= 10;
+			tmp =k;
+			k=j+k;
+			j=tmp;
+			partialresult=partial1+partial2;
+			if(partialresult>9)
+			{
+				partialresult=((k%10)*10)+(partialresult);
+				
+			}
+			printf("f(%d) %lu%d",i,k,partialresult);
+		}
+		else{
+		printf("f(%i) %lu",i,k);
+		}
 		tmp = k;
 		k = j + k;
 		j = tmp;
 		start = 1;
+		
 	}
 	printf("\n");
 	return (0);
