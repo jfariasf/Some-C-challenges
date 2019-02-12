5a07684cc0cebbf7a0f4696b8080faa7da691023
Javier F. Arias <752@holbertonschool.com>
2019-02-12 07:20:56 -0500

    Fixing 0-holberton.c

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,15 @@
+/**
+* main - Prints "Holberton"
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char msg[] = "Holberton";
+	unsigned int i;
+
+	for (i = 0; i < sizeof(msg) - 1; i++)
+		_putchar(msg[i]);
+	_putchar(0x0A);
+	return (0);
+}
