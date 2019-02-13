47e663d6074080c4b29891a965bed369d568c1b2
Javier F. Arias <752@holbertonschool.com>
2019-02-12 23:19:06 -0500

    Fixing 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -26,10 +26,10 @@ int main(void)
 		{
 			if (start2 == 0)
 			{
-				k1 = (int) (k % 1000);
-				j1 = (int) (j % 1000);
-				k /= 1000;
-				j /= 1000;
+				k1 = (int) (k % 10000);
+				j1 = (int) (j % 10000);
+				k /= 10000;
+				j /= 10000;
 			}
 			tmp2 = k1;
 			k1 = j1 + k1;
@@ -54,13 +54,13 @@ int main(void)
 */
 void calculate_last(unsigned long k, int k1)
 {
-	if (k1 > 999)
+	if (k1 > 9999)
 	{
-		k += 1;
-		k1 %= 1000;
+		k += k1 / 10000;
+		k1 %= 10000;
 	}
 	if (k1 > 0)
-		printf("%lu%03d", k, k1);
+		printf("%lu%04d", k, k1);
 	else
 		printf("%lu", k);
 }
