12c627b8ff8cf863b6a29e5b1e39b516e5241ff1
Javier F. Arias <752@holbertonschool.com>
2019-02-14 14:27:37 -0500

    Adding 2-mul.c

---
+++ 0x03-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,10 @@
+/**
+* mul - Returns the result of a multiplication
+* @a: parameter 1
+* @b: parameter 2
+* Return: The result of the multiplication as an integer
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
