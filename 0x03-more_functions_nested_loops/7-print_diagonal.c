831233c0f8ebf9c1dc88d9da5b18ee9549698052
Javier F. Arias <752@holbertonschool.com>
2019-02-14 14:14:51 -0500

    Adding 7-print_diagonal.c

--- 0x03-more_functions_nested_loops/7-print_diagonal.c
+++ 0x03-more_functions_nested_loops/7-print_diagonal.c
@@ -17,7 +17,8 @@ void print_diagonal(int n)
 	for (i = 0; i < n; i++)
 	{
 		for (j = 0; j < n; j++)
-			_putchar((i == j) ? '\\' : ' ');
+			if(j<=i)
+				_putchar((j == i) ? '\\' : ' ');
 		_putchar(0x0A);
 	}
 }
