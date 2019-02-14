b1f0a70b9c27868a6c32511cdcf8feb59edb17fa
Javier F. Arias <752@holbertonschool.com>
2019-02-14 14:26:10 -0500

    Adding 3-print_numbers.c

---
+++ 0x03-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+* print_numbers - Prints the numbers, from 0 to 9
+*
+* Return: Nothing
+*/
+void print_numbers(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+		_putchar(i);
+	_putchar(0x0A);
+}
