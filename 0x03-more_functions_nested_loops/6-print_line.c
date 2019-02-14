e43d940a31e134c625a4b678f48debe8fa6dd1c6
Javier F. Arias <752@holbertonschool.com>
2019-02-14 14:17:51 -0500

    Adding 6-print_line.c

---
+++ 0x03-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include "holberton.h"
+/**
+* print_line - Draws a straight line in the terminal
+* @n: number of times the character '_' should be printed
+* Return: nothing
+*/
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+		_putchar('_');
+	_putchar(0x0A);
+}
