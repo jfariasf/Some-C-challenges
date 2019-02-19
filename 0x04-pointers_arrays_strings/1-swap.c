04edc16c26f5243e15fe0044860f4dc99746832e
Javier F. Arias <752@holbertonschool.com>
2019-02-19 15:17:07 +0000

    Fixing betty 1-swap.c

--- 0x04-pointers_arrays_strings/1-swap.c
+++ 0x04-pointers_arrays_strings/1-swap.c
@@ -1,12 +1,14 @@
 #include "holberton.h"
 
 /**
- * swap_int - Takes a pointer to an int as parameter and updates the value it points to to 98
- * Return: Always 0.
+ * swap_int - Swaps the value of two integers
+ * @a: First int argumen
+ * @b: Secong int argument
+ * Return: Nothing.
  */
 void swap_int(int *a, int *b)
 {
 	int temp = *b;
 	*b = *a;
-	*a =temp;
+	*a = temp;
 }
