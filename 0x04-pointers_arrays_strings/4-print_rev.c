6ed5cc795fbac2e07616f9d623c3ee68a9dc86d5
Javier F. Arias <752@holbertonschool.com>
2019-02-19 15:27:04 +0000

    Updating betty for 4-print_rev.c

--- 0x04-pointers_arrays_strings/4-print_rev.c
+++ 0x04-pointers_arrays_strings/4-print_rev.c
@@ -1,19 +1,21 @@
 #include "holberton.h"
 
 /**
- * print_rev - Takes a pointer to an int as parameter and updates the value it points to to 98
- * Return: Always 0.
+ * print_rev - Prints a sting in reverse
+ * @str: the input string
+ * Return: Nothing.
  */
 void print_rev(char *str)
 {
 	int count = 0;
-	while(*str!='\0')
+
+	while (*str != '\0')
 	{
 		count++;
 		str++;
 	}
 	str--;
-	while(count>0)
+	while (count > 0)
 	{
 		_putchar(*str);
 		str--;
