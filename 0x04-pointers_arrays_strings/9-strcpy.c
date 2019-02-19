35b813f206a1ed9a9eb58ecf98fb2b96b76c9ca6
Javier F. Arias <752@holbertonschool.com>
2019-02-19 07:03:56 -0500

    Adding 9-strcpy.c

---
+++ 0x04-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,43 @@
+#include "holberton.h"
+char *seek(char *, int);
+/**
+ * _strcpy - Copy a string to a buffer and returns the dest buffer.
+ * @dest: the destination buffer
+ * @src: the source buffer
+ * Return: the destionation buffer.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int count = 0;
+	char *bkdest = dest;
+
+	while (*src != '\0')
+	{
+		count++;
+		src++;
+	}
+	src = seek(src, -(count));
+	while (count > 0)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+		count--;
+	}
+	return (bkdest);
+}
+/**
+ * seek - Move a char pointer to the desired position
+ * @p: a char pointer
+ * @pos: positiong to move the pointer to
+ * Return: the pointer moved.
+ */
+char *seek(char *p, int pos)
+{
+	while (pos != 0)
+	{
+		((pos > 0) ? (p++ && pos--) : (p-- && pos++));
+
+	}
+	return (p);
+}
