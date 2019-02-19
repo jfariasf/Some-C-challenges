ab998b0d4c4f90644f0a9af2ff683343d4a1782e
Javier F. Arias <752@holbertonschool.com>
2019-02-19 15:21:49 +0000

    Fixing 2-strlen.c

--- 0x04-pointers_arrays_strings/2-strlen.c
+++ 0x04-pointers_arrays_strings/2-strlen.c
@@ -1,17 +1,18 @@
 #include "holberton.h"
-void _putchar(int);
 
 /**
- * _strlen - Takes a pointer to an int as parameter and updates the value it points to to 98
- * Return: Always 0.
+ * _strlen - Prints the length of a string
+ * @s: Pointer to a char array
+ * Return: The length of the array/length of string.
  */
 int _strlen(char *s)
 {
-	int size=0;
-	while(*s!='\0')
+	int size = 0;
+
+	while (*s != '\0')
 	{
 		s++;
 		size++;
 	}
-	return size;
+	return (size);
 }
