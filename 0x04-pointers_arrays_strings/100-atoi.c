ed139909055e04db5f1e6d6a9e963a469726dbe3
Javier F. Arias <752@holbertonschool.com>
2019-02-20 08:26:33 -0500

    Feeling sick but completed 100-atoi.c

--- 0x04-pointers_arrays_strings/100-atoi.c
+++ 0x04-pointers_arrays_strings/100-atoi.c
@@ -1,36 +1,61 @@
 #include "holberton.h"
-
+#include <stdio.h>
+int power(int, int);
 /**
- * _atoi - Takes a pointer to an int as para
- * Return: Always 0.
+ * _atoi - Convert a string to an integer
+ * @s: Pointer to a string
+ * Return: The number converter to integer.
  */
 int _atoi(char *s)
 {
 	int a = 1;
 	int counter = 0;
 	int init = 0;
+	int intvalue = 0;
 
-	while(*str!='\0')
+	while (*s != '\0')
 	{
-		if (*str>='0' && *str<='9')
+		if (*s >= '0' && *s <= '9')
 		{
-			(init != count) ? init = 1:;
-			counter += 1;
-		}
-		else
-		{
-			
-		if (*str=='-')
-			a=-(a);
-		else if (*str=='+')
-			a=a;
-		
-			
-		}
+			if (init > counter && counter == 0)
+				init = counter;
+			counter++;
 
-		_putchar(*str);
-		str++;
+		}
+		else if (*s == '-')
+			a = -(a);
+		s++;
 		init++;
+		if (init > counter && counter != 0)
+		{
+			s--;
+			break;
+		}
 	}
-	_putchar('\n');
+	while (counter > 0)
+	{
+		intvalue += (*(s - counter) - 0x30) * power(10, counter - 1);
+		counter--;
+	}
+	return (intvalue * a);
+}
+
+/**
+ * power - Raise a number to a power
+ * @number: Number to raise
+ * @raise: Raise to this number
+ * Return: The number raise to the specified power.
+ */
+int power(int number, int raise)
+{
+	int power = number;
+
+	if (raise == 0)
+		return (1);
+	while (raise > 1)
+	{
+		power *= number;
+		raise--;
+	}
+	return (power);
 }
