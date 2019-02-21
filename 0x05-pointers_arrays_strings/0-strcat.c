3c744183a6b01d5a34a346b88865d607e33a2ae1
Javier F. Arias <752@holbertonschool.com>
2019-02-21 18:07:14 -0500

    Helping with 0-strcat.c

---
+++ 0x05-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+char *seek(char *, int);
+/**
+ * _strcat - Copy a string to a buffer and returns the dest buffer.
+ * @dest: the destination buffer
+ * @src: the source buffer
+ * Return: the destionation buffer.
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *bkdest = dest;
+
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*src = '\0';
+	return (bkdest);
+}
