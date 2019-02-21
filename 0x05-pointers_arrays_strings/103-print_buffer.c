d261e943a712d085ae378467d594eaaab2682900
Javier F. Arias <752@holbertonschool.com>
2019-02-21 17:56:49 -0500

    Testing 103-print_buffer.c

--- 0x05-pointers_arrays_strings/103-print_buffer.c
+++ 0x05-pointers_arrays_strings/103-print_buffer.c
@@ -3,75 +3,77 @@
 void gethexvals(char *, int);
 void printstring(char *, int);
 /**
- * _atoi - Convert a string to an integer
- * @s: Pointer to a string
+ * print_buffer - Convert a string to an integer
+ * @b: Pointer to a string
+ * @size: size
  * Return: The number converter to integer.
  */
 void print_buffer(char *b, int size)
 {
-	int row=0x00;
-	int *test;
+	int row = 0x00;
+	int ln = 10;
 
-	char buf[10];
-	int i=0;
-	int ln=10;
-
-	while (size>0)
+	if (size <= 0)
+	{
+		printf("\n");
+		return;
+	}
+	while (size > 0)
 	{
 
-		printf("%08x: " , row);
-		gethexvals(b,ln);
+		printf("%08x: ", row);
+		gethexvals(b, ln);
 		putchar(' ');
-		printstring(b,ln);
+		printstring(b, ln);
 		putchar('\n');
-		row+=10;
-		b+=10;
-		size-=10;
+		row += 10;
+		b += 10;
+		size -= 10;
 
-		if (size<10)
-			ln=size;
+		if (size < 10)
+			ln = size;
 	}
 }
-void gethexvals(char *c,int sz)
+/**
+ * gethexvals - Convert a string to an integer
+ * @c: Pointer to a string
+ * @sz: size
+ * Return: The number converter to integer.
+ */
+
+void gethexvals(char *c, int sz)
 {
 	char buf[30];
-	char *p=buf;
-	int sep=0;
+	char *p = buf;
 	int i;
-	for (i=0; i< sz;i++)
+
+	for (i = 0; i < sz; i++)
 	{
-		if (i%2==0 && i>0)
+		if (i % 2 == 0 && i > 0)
 		{
-			sprintf(p,"%c",0x20);
+			sprintf(p, "%c", 0x20);
 			p++;
 		}
-		sprintf(p,"%02x",*c);
-		p+=2;
-		c++;	
+		sprintf(p, "%02x", *c);
+		p += 2;
+		c++;
 	}
-	printf("%-24s",buf);
-	/*
-	if (sz<10)
-	{
-		
-		for (i=14-sz;i>0;i--)
-		{
-			if(i%2==0)
-				putchar(' ');
-			printf("  ");
-			i--;
-		}
-	}
-	*/
-		
+	printf("%-24s", buf);
 }
+/**
+ * printstring - Convert a string to an integer
+ * @c: Pointer to a string
+ * @sz: size
+ * Return: The number converter to integer.
+ */
 void printstring(char *c, int sz)
 {
 	int i;
-	for(i=0;i<sz;i++)
+
+	for (i = 0; i < sz; i++)
 	{
-		if (*c>=0x20 && *c<=0x7e)
-			printf("%c",*c);
+		if (*c >= 0x20 && *c <= 0x7e)
+			printf("%c", *c);
 		else
 			putchar('.');
 		c++;
