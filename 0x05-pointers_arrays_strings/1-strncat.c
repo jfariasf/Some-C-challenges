fa00c4294ff8acbcc4c8e78cdcb5a709924b911d
Javier F. Arias <752@holbertonschool.com>
2019-02-21 18:29:49 -0500

    Adding 1-strncat.c

---
+++ 0x05-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+/**
+ * _strncat - Copy a string to a buffer and returns the dest buffer.
+ * @dest: the destination buffer
+ * @src: the source buffer
+ * Return: the destionation buffer.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	char *bkdest = dest;
+	char *bksrc = src;
+	int count = 0;
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+	while (*src != '\0')
+	{
+		src++;
+		count++;
+	}
+	if (n>count)
+		n=count;
+	while (n > 0)
+	{
+		*dest = *bksrc;
+		dest++;
+		bksrc++;
+		n--;
+	}
+	return (bkdest);
+}
