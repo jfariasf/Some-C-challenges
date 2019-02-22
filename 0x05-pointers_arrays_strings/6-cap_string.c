fbda0e5ab69c37f62e4a88232edf9a2674f773da
Javier F. Arias <752@holbertonschool.com>
2019-02-21 23:04:12 -0500

    Adding 6-cap_string.c

---
+++ 0x05-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * cap_string - Capitalize a string.
+ * @dest: the stringr
+ * Return: the destionation buffer.
+ */
+char *cap_string(char *dest)
+{
+	char *bkdest = dest;
+	char sep[13] = " \t\n,;.!?\"(){}";
+	int cap = 0;
+	int i;
+
+	while (*dest != '\0')
+	{
+		for (i = 0; i < 13; i++)
+		{
+			if (*dest == sep[i])
+			{
+				cap = 1;
+				break;
+			}
+		}
+		if (*dest >= 0x61 && *dest <= 0x7A && cap == 1)
+		{
+			*dest -= 0x20;
+			cap = 0;
+		}
+		else if (*dest >= 0x41 && *dest <= 0x5A)
+			cap = 0;
+		dest++;
+	}
+	return (bkdest);
+}
