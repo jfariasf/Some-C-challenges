a5b711271aae47ca4548e372741423d28cd1fa64
Javier F. Arias <752@holbertonschool.com>
2019-03-05 20:08:40 -0500

    Bored. Testing

--- 0x0A-malloc_free/0-create_array.c
+++ 0x0A-malloc_free/0-create_array.c
@@ -15,6 +15,8 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 	array = (char *) malloc(sizeof(char) * size);
+	if (array == NULL)
+		return (NULL);
 	for (i = 0; i < (int) size; i++)
 		array[i] = c;
 	return (array);
