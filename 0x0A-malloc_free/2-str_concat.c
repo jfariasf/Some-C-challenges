c908295e72bc5b8fd2aa1bd8aa0e82c1c85a8c47
Javier F. Arias <752@holbertonschool.com>
2019-03-06 00:48:13 -0500

    Bored

--- 0x0A-malloc_free/2-str_concat.c
+++ 0x0A-malloc_free/2-str_concat.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 /**
  * str_concat - Concat 2 strings
  * @s1: string 1
@@ -9,21 +10,27 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *array;
-	int sz1, sz2, i, j = 0;
+	int sz1 = 0;
+	int sz2 = 0;
+	int i, j;
 
 	if (s1 != NULL)
+	{
 		for (sz1 = 0; s1[sz1]; sz1++)
 		;
+	}
 	if (s2 != NULL)
+	{
 		for (sz2 = 0; s2[sz2]; sz2++)
 		;
-
-	array = (char *) malloc(sizeof(char) * (sz1 + sz2 + 1));
+	}
+	array = (char *) malloc(sizeof(char) * (sz1 + sz2) + 1);
 	if (array == NULL)
 		return (NULL);
 	for (i = 0; i < sz1; i++)
 		array[i] = s1[i];
-	for (j = 0; j <= sz2; j++, i++)
+	for (j = 0; j < sz2; j++, i++)
 		array[i] = s2[j];
+	array[i] = '\0';
 	return (array);
 }
