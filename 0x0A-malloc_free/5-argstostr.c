756de05af894b52d877739aca43069023e28b447
Javier F. Arias <752@holbertonschool.com>
2019-03-05 21:10:46 -0500

    Bored adding 5

---
+++ 0x0A-malloc_free/5-argstostr.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * argstostr - Arguments to string
+ * @ac: Argument count
+ * @av: The array or args
+ *
+ * Return: the single array
+ */
+char *argstostr(int ac, char **av)
+{
+	char *array, *test;
+	int i, count;
+
+	test = *(av);
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	while (ac > 0)
+	{
+		if (*test == '\0')
+		{
+			ac--;
+		}
+		test++;
+		count++;
+	}
+	test -= count;
+	array = (char *) malloc(sizeof(char) * count + 1);
+	if (array == NULL)
+		return (NULL);
+	for (i = 0; i < count; i++)
+		if (test[i] == '\0')
+			array[i] = '\n';
+		else
+			array[i] = test[i];
+	return (array);
+}
