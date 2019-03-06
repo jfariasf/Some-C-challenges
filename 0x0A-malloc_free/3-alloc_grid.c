5fc96b35fefd44deda1613785b6a38ded4744efd
Javier F. Arias <752@holbertonschool.com>
2019-03-06 09:26:38 -0500

    Bored

--- 0x0A-malloc_free/3-alloc_grid.c
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -1,9 +1,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
- * str_concat - Concat 2 strings
- * @s1: string 1
- * @s2: string 2
+ * alloc_grid - Concat 2 strings
+ * @width: string 1
+ * @height: string 2
  *
  * Return: the array
  */
@@ -16,12 +16,12 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || width <= 0)
 		return (NULL);
 
-	harray = (int **) malloc(sizeof(int **) * height + 1);
+	harray = (int **) malloc(sizeof(int **) * height);
 	if (harray == NULL)
 		return (NULL);
 	for (i = 0; i < height; i++)
 	{
-		harray[i] = (int *) malloc(sizeof(int) * width + 1);
+		harray[i] = (int *) malloc(sizeof(int) * width);
 		if (harray[i] == NULL)
 			return (NULL);
 		for (j = 0; j < width; j++)
